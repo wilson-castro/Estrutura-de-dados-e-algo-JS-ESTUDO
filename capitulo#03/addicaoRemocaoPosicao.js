63817c9a2257fe8d86804e9ffc4388f6dbeff928
wilson-castro <wilsoncastro.pv@gmail.com>
2022-01-22 07:01:48 +0000

    Adição e remoção de arrays em posições específicas

---
+++ capitulo#03/addicaoRemocaoPosicao.js
@@ -0,0 +1,9 @@
+let numbers = [-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13];
+
+// Remove 3 elemntos a partir do índice 6
+numbers.slice(6,3);
+
+// a partir do índice 6 remova 0 e adicione os números 2,3 e 4
+numbers.slice(6,0,2,3,4);
+
+numbers.slice(5,3,2,3,4);

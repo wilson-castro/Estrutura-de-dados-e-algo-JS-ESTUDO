2d852add3c775bf1006bcc008f9efc1fd59a56ab
wilson-castro <wilsoncastro.pv@gmail.com>
2022-01-22 06:20:29 +0000

    Estudo da inicialização de arays e teste com a sequência de Fibonacci

---
+++ capitulo#03/incializandoArray.js
@@ -0,0 +1,5 @@
+const temAvarage = new Array();
+const temperatuaMensa = new Array(7);
+const temMen = new Array("janeiro, Fevereiro, Março, Abril, Maio, Junho, Julho");
+const tempoeraturaSemana = [];
+const temperature = [90,20,20,202,0];
---
+++ capitulo#03/sequenciaFibonacci.js
@@ -0,0 +1,9 @@
+const fibonacci = [];
+fibonacci[1] = 1;
+fibonacci[2] = 2;
+
+for(let i = 3; i <  20; i++) {
+  fibonacci[i] = fibonacci[i -1] + fibonacci[i - 2];
+}
+
+console.log(fibonacci);

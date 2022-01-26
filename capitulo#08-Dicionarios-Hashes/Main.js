82b1b6c8fcb6fd5a99256407780365cc29817c39
wilson-castro <wilsoncastro.pv@gmail.com>
2022-01-26 20:22:37 +0000

    Testes com dicionários

--- capitulo#08-Dicionarios-Hashes/Main.js
+++ capitulo#08-Dicionarios-Hashes/Main.js
@@ -98,3 +98,26 @@ class Dictionary {
     return objString;
   }
 }
+
+
+dictionary.set('Gandalf', 'gandalf@email.com');
+dictionary.set('John', 'johnsnow@email.com');
+dictionary.set('Tyrion', 'tyrion@email.com');
+
+console.log(dictionary.hasKey('Gandalf')); 
+console.log(dictionary.size()); 
+console.log(dictionary.keys()); 
+console.log(dictionary.values()); 
+console.log(dictionary.get('Tyrion')); 
+
+dictionary.remove('John');
+
+console.log(dictionary.keys()); 
+console.log(dictionary.values()); 
+
+console.log(dictionary.keyValues()); 
+console.log(dictionary.toString()); 
+
+dictionary.forEach((k, v) => {
+  console.log('forEach: ', `key: ${k}, value: ${v}`);
+});

ea4533f280d21728d80e78ebf222bf94df3f3817
wilson-castro <wilsoncastro.pv@gmail.com>
2022-01-30 07:26:53 +0000

    Estudo do estudo das DFS

--- capitulo#12-grafos/Main.js
+++ capitulo#12-grafos/Main.js
@@ -191,3 +191,38 @@ for (let i = 1; i < myVertices.length; i++) {
   }
   console.log(s);
 }
+
+graph = new Graph(true); // directed graph
+
+myVertices = ['A', 'B', 'C', 'D', 'E', 'F'];
+for (i = 0; i < myVertices.length; i++) {
+  graph.addVertex(myVertices[i]);
+}
+graph.addEdge('A', 'C');
+graph.addEdge('A', 'D');
+graph.addEdge('B', 'D');
+graph.addEdge('B', 'E');
+graph.addEdge('C', 'F');
+graph.addEdge('F', 'E');
+
+const result = DFS(graph);
+console.log('discovery', result.discovery);
+console.log('finished', result.finished);
+console.log('predecessors', result.predecessors);
+
+const fTimes = result.finished;
+s = '';
+for (let count = 0; count < myVertices.length; count++) {
+  let max = 0;
+  let maxName = null;
+  for (i = 0; i < myVertices.length; i++) {
+    if (fTimes[myVertices[i]] > max) {
+      max = fTimes[myVertices[i]];
+      maxName = myVertices[i];
+    }
+  }
+  s += ' - ' + maxName;
+  delete fTimes[maxName];
+}
+console.log(s);
+
